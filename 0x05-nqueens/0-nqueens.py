93da0f823e976e1184af65e9c94494692e076cad
Maged Saif <magedsaif21@gmail.com>
2024-02-09 14:35:00 +0200

    task 0

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""N queens puzzle"""
+import sys
+
+
+def isSafe(board, row, col, n):
+    """Check if a queen can be placed on board[row][col]"""
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
