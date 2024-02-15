7ec1cfc565a0c503b498881ac323357133fa3ed0
magedsaif <magedsaif21@gmail.com>
2024-02-15 17:10:08 +0200

    task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,45 +1,20 @@
+#!/usr/bin/node
+
 const request = require("request");
 
-const baseURL = "https://swapi-api.hbtn.io/api/films/";
-
-request(baseURL + process.argv[2], (err, res, body) => {
-  if (err) {
-    console.error("Error fetching movie data:", err);
-    process.exit(1);
+request(
+  "https://swapi-api.hbtn.io/api/films/" + process.argv[2],
+  function (err, res, body) {
+    if (err) throw err;
+    const actors = JSON.parse(body).characters;
+    exactOrder(actors, 0);
   }
-
-  if (res.statusCode !== 200) {
-    console.error("Error:", res.statusCode);
-    process.exit(1);
-  }
-
-  const filmData = JSON.parse(body);
-  const characterUrls = filmData.characters;
-
-  const characterRequests = characterUrls.map((url) => {
-    return new Promise((resolve, reject) => {
-      request(url, (error, response, characterBody) => {
-        if (error) {
-          reject(error);
-          return;
-        }
-
-        if (response.statusCode !== 200) {
-          reject(`Error fetching character ${url}: ${response.statusCode}`);
-          return;
-        }
-
-        const character = JSON.parse(characterBody);
-        resolve(character);
-      });
-    });
+);
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
   });
-
-  Promise.all(characterRequests)
-    .then((characters) => {
-      characters.forEach((character) => console.log(character.name));
-    })
-    .catch((error) => {
-      console.error("Error fetching characters:", error);
-    });
-});
+};
