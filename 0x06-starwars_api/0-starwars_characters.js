3b459bb0c7cf522497f1e1853eb6474a918019fb
magedsaif <magedsaif21@gmail.com>
2024-02-15 17:14:07 +0200

    task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
+
 const util = require('util');
 const request = util.promisify(require('request'));
 const filmID = process.argv[2];
 
-async function starwarsCharacters(filmId) {
+async function starwarsCharacters (filmId) {
   const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
   let response = await (await request(endpoint)).body;
   response = JSON.parse(response);
